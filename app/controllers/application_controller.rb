f315b97dc9639ed264f9dd5f9fff880b2b90377a
Toffy Caluga <abraham.lillol@gmail.com>
2023-07-20 15:43:18 -0300

    FEAT:photos scaffold creada

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:profile_photo])
+  end
 end
