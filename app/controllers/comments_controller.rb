6b9107982b40067b9ee85de2660ae9ebfec6b069
Toffy Caluga <abraham.lillol@gmail.com>
2023-07-21 14:54:34 -0300

    FEAT:Comment controller creado

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,33 @@
+class CommentsController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+      @commentable = find_commentable
+      @comment = @commentable.comments.build(comment_params)
+      @comment.user = current_user
+  
+      if @comment.save
+        redirect_to commentable_path, notice: 'Comentario creado correctamente.'
+      else
+        redirect_to commentable_path, alert: 'No se pudo crear el comentario.'
+      end
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:content, :commentable_id, :commentable_type, :user_id)
+    end
+  
+    def commentable_path
+      # Lógica para redirigir a la página de detalle de Photo o User, dependiendo del tipo de comentario
+    end
+  
+    def find_commentable
+      if params[:commentable_type] == 'Photo'
+        Photo.find(params[:commentable_id])
+      elsif params[:commentable_type] == 'User'
+        User.find(params[:commentable_id])
+      end
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :photos
   devise_for :users
+  resources :photos do
+    resources :comments, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
