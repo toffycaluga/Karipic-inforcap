bd15c2f697651264ccc4aa03861e24fa2450724f
Toffy Caluga <abraham.lillol@gmail.com>
2023-07-21 09:18:19 -0300

    FEAT:Tamaño de imagen on resize

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :user
-  has_one_attached :photo
+  has_one_attached :photo do |attachable|
+    attachable.variant :thumb,resize_to_limit:[200,300]
+  end
 end
